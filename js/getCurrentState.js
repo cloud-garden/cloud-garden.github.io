ca3643c85e761bb089225bed7e99b5a02795c925
tbonFukitama <tbonfukitama110@gmail.com>
2015-10-30 05:46:59 +0900

    make character message

--- js/getCurrentState.js
+++ js/getCurrentState.js
@@ -6,5 +6,19 @@ $(function(){
 		$("#date").text((date.getFullYear())+"年"+(date.getMonth()+1)+"月"+(date.getDate())+"日");
 		$("#temperature").text(json.temp);
 		$("#humidity").text(json.humid);
+		updateCharacter(json.temp, json.humid);
 	});
 });
+
+function updateCharacter(temp, hum) {
+	var message = Math.random() > 0.5 ? "今日はいい天気！<br>気持ちいいっちゃ！" : "今日も頑張ろっちゃ！";
+	if(temp > 28) {
+		message = "あっついね．<br>氷を食べたいっちゃ！";
+	} else if(temp < 14) {
+		message = "寒い〜。<br>元気が無いっちゃ・・・";
+	}
+	if(hum < 30) {
+		message = "乾燥してるよ！<br>喉が渇いたっちゃ〜。";
+	}
+	$("#character-comment p").html(message);
+}
