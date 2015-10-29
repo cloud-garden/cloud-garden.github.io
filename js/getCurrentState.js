5047a46d4fc7d4a0adc2e155e10fedb870845c13
tbonFukitama <tbonfukitama110@gmail.com>
2015-10-30 06:09:31 +0900

    change character face

--- js/getCurrentState.js
+++ js/getCurrentState.js
@@ -11,14 +11,40 @@ $(function(){
 });
 
 function updateCharacter(temp, hum) {
+
+	//debug mode
+	if(getQuerystring("d") == "y") {
+		temp = 40*Math.random();
+		hum = 60*Math.random();
+		$("#temperature").text(Math.floor(temp));
+		$("#humidity").text(Math.floor(hum));		
+	}
+
+
 	var message = Math.random() > 0.5 ? "今日はいい天気！<br>気持ちいいっちゃ！" : "今日も頑張ろっちゃ！";
+	var imgURL = "default";
 	if(temp > 28) {
 		message = "あっついね．<br>氷を食べたいっちゃ！";
+		imgURL = "surprise";
 	} else if(temp < 14) {
 		message = "寒い〜。<br>元気が無いっちゃ・・・";
+		imgURL = "sad";
 	}
 	if(hum < 30) {
 		message = "乾燥してるよ！<br>喉が渇いたっちゃ〜。";
+		imgURL = "angry";
 	}
 	$("#character-comment p").html(message);
+  $("#character-img").css({"background-image": "url(img/home/character-"+imgURL+".png)"});
 }
+
+function getQuerystring(key, default_)  {  
+   if (default_==null) default_="";  
+   key = key.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");  
+   var regex = new RegExp("[\\?&]"+key+"=([^&#]*)");  
+   var qs = regex.exec(window.location.href);  
+   if(qs == null)  
+    return default_;  
+   else  
+    return qs[1];  
+}  
--- js/getPhoto.js
+++ js/getPhoto.js
@@ -5,7 +5,6 @@ $(function(){
     checkIsMobile();
     if(isMobile) {
         $("#main-content").css({"background-image": "url(img/home/dummy.jpg)"});
-        $("#character-img").css({"background-image": "url(img/home/character.png)"});
     }
 
     function checkIsMobile() {
