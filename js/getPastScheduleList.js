112aff9de64e1c4eface7eb11c71126f50b687fc
unknown <okashige.masatoshi@g.wakayama-u.jp>
2015-11-04 21:00:24 +0900

    過去リスト表示＆UI修正

--- js/getPastScheduleList.js
+++ js/getPastScheduleList.js
@@ -1,20 +1,36 @@
-$(function(){
-	var sendData = {user:"user1"};
-	$.getJSON("http://54.199.139.148/cloud_garden_server/api/getPastScheduleList", sendData, function(json){
-		var arry = new Array([]);
-		for (var i = 0; i < arry.length; i++){
-			//
-			date = new date(Number(schedule[i].date));
-			$("#date").text((date.getFullYear())+"年"+(date.getMonth()+1)+"月"+(date.getDate())+"日");
+var debugCounter = 0; //デバッグ用
+var prevCounter = 0; //遡った回数
 
-			var Botan = document.createElement("input");
-			var create = document.getElementById("activeschedule");
-			create.appendChild(Botan);
-			Botan.appendChild(past);
-			Botan.type = "button";
-			Botan.value = "ボタン";
-			Botan.id = "BotanID";
-			Botan.onclick = "delet";
-		}
-	});
+$(function(){
+	var date = new Date();
+	var millisec = date.getTime();
+		var sendData = {user:"keita", date:(millisec - 86400000)};
+		// var sendData = {user:"user1", date:(millisec)};
+		setTimeout(function(data) { loadPastScheduleList(data, 0); }, 100, sendData);
 });
+
+function loadPastScheduleList(sendData, zIndex) {
+	$.getJSON("http://54.199.139.148/cloud_garden_server/api/getPastScheduleList", sendData, function(json){
+			pastScheduleList(json.date);
+		});
+}
+
+function createHtml(json, zIndex) {
+	var html =
+		"<div class=\"logconts2\" id=\"logconts2-"+json.date+"\"></div>";
+	return html;
+}
+
+function pastScheduleList(millisec) {
+	var sendData = {user:"keita"};
+	$.getJSON("http://54.199.139.148/cloud_garden_server/api/getPastScheduleList", sendData, function(json) {
+		// console.log(json);
+		$.each(json.schedule, function(index, item) {
+			// var date = item.date;
+			// console.log(index + " : "+ item);
+			var date = new Date(parseInt(item.date));
+			$("#logconts2 ul").append("<li>" + (date.getFullYear()) + "年"+(date.getMonth()+1)+"月"+(date.getDate())+"日 "+(date.getHours())+":"+(date.getMinutes())+"</li>");
+			// $("#log-content-"+millisec).append("")
+		});
+	});
+}
--- js/schedule.js
+++ js/schedule.js
@@ -5,10 +5,14 @@
 $("#active").click(function() {
 	$("#activeschedule").show();
 	$("#pastschedule").hide();
+	$("li.active").css('background','#CD853F');
+	$("li.past").css('background','#8C775F');
 });
 
 $("#past").click(function() {
 	$("#activeschedule").hide();
 	$("#pastschedule").show();
+	$("li.past").css('background','#CD853F');
+	$("li.active").css('background','#8C775F');
 });
 
