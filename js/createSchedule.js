b4e4f38dbab96299bc12ed302f897766031b82ad
unknown <okashige.masatoshi@g.wakayama-u.jp>
2015-11-09 23:40:21 +0900

    create createschedule

--- js/createSchedule.js
+++ js/createSchedule.js
@@ -1,14 +1,23 @@
 $(function() {
   $("#create-schedule").click(function(){
-  	var dateStr = $("#feed-date").val();
+  	var dateStr = $("#datepicker").val();
   	var timeStr = $("#feed-time").val();
   	var routine = $("#feed-routine").prop('checked');
-
-  	var date = new Date(dateStr+"T"+timeStr+":00+0900");
-  	var sendData = {date:date.getTime(), user:"keita", isRoutine:routine};
-  	$.getJSON("http://54.199.139.148/cloud_garden_server/api/createSchedule", sendData, function(json){
-			console.log(json);
+	//console.log(dateStr);
+	//console.log(timeStr);
+	//console.log(routine);
+	//var date = new Date(dateStr+"T"+timeStr+":00+0900");
+  	var date = Date.parse(dateStr+" "+timeStr+":00");
+  	var sendData = {date:date, user:"keita", isRoutine:routine};
+  	var now = new Date();
+  	if(date > now.getTime()){
+ 	$.getJSON("http://54.199.139.148/cloud_garden_server/api/createSchedule", sendData, function(json){
+			window.location.reload(true);
+			alert("登録完了しました");
 		});
     return false;
+    }else{
+    	alert("日付が過去です");
+    }
   });
 });
--- js/deleteSchedule.js
+++ js/deleteSchedule.js
@@ -2,8 +2,8 @@ $(function() {
 $("body ul").on("click","a",function(){
 	var scheduleId = $(this).attr('id');
   	var sendData = {id:scheduleId};
-  	console.log(sendData);
  	$.getJSON("http://54.199.139.148/cloud_garden_server/api/deleteSchedule", sendData, function(json){
+			window.location.reload(true);
 			alert("削除しました");
 		});
     return false;
