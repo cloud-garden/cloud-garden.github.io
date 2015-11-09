87e481188600d1fe862020d44e0f7923d01f0c7f
unknown <okashige.masatoshi@g.wakayama-u.jp>
2015-11-10 00:19:58 +0900

    操作の不具合を修正

--- js/deleteSchedule.js
+++ js/deleteSchedule.js
@@ -1,5 +1,5 @@
 $(function() {
-$("body ul").on("click","a",function(){
+$("body div.page1 ul").on("click","a",function(){
 	var scheduleId = $(this).attr('id');
   	var sendData = {id:scheduleId};
  	$.getJSON("http://54.199.139.148/cloud_garden_server/api/deleteSchedule", sendData, function(json){
