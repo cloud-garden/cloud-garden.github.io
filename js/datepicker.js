f60ff274e5a4c39ef3413bf4dc79e1bc39d575f2
unknown <okashige.masatoshi@g.wakayama-u.jp>
2015-11-11 18:11:05 +0900

    change button

--- js/datepicker.js
+++ js/datepicker.js
@@ -6,6 +6,6 @@ $(function() {
     $.datepicker.setDefaults( $.datepicker.regional[ "ja" ] );
     $( "#datepicker" ).datepicker();
     $("#datepicker").datepicker("option","showOn",'button');
-    $("#datepicker").datepicker("option","buttonImageOnly",true);
-    $("#datepicker").datepicker("option","buttonImage",'./img/recipe/calender.png');
+    //$("#datepicker").datepicker("option","buttonImageOnly",true);
+    //$("#datepicker").datepicker("option","buttonImage",'./img/recipe/calender.png');
   });
