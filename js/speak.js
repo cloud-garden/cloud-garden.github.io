3e4aa153f0e98556b0909e00ed7558176a24c37d
tbonFukitama <tbonfukitama110@gmail.com>
2015-11-11 22:43:11 +0900

    ハーベス子しゃべる

---
+++ js/speak.js
@@ -0,0 +1,29 @@
+var isSpeaking = false;
+
+$(function(){
+
+    if (!'SpeechSynthesisUtterance' in window) {
+        console.log('Speech synthesis(音声合成) APIには未対応です.');
+        return;
+    }
+
+    var msg = new SpeechSynthesisUtterance();
+    var voices = window.speechSynthesis.getVoices();
+
+    msg.voice = voices[7];
+    msg.volume = 1.0;
+    msg.rate = 1.2;
+    msg.pitch = 1.3;
+    msg.lang = 'ja-UP';
+    msg.onend = function (event) {
+    	isSpeaking = false;
+    }
+
+		$("#character-img").click(function() {
+			if(!isSpeaking) {
+				isSpeaking = true;
+		    msg.text = $("#character-comment p").text();
+		    speechSynthesis.speak(msg);
+			}
+		});
+});
